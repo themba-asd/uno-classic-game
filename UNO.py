6a297787374c43b1f69ff05e1e98b126316291a5
thembalakhe <m.themba1738@gmail.com>
2024-09-20 00:05:41 +0200

    program can now create deck and set the bottom card

--- UNO.py
+++ UNO.py
@@ -1,12 +1,10 @@
-#UNO
+#UNO Nton Nton
+import random as r
 
 colors = ["red", "yellow", "blue", "green"]
+deck: list[dict] = [] #{}
 
-deck = [#{
-  #   "symbol": "",
-  #   "color": "",
-  # }
-]
+bottom_card = ''
 
 def create_deck_cards(start, end = 10):
 
@@ -18,6 +16,7 @@ def create_deck_cards(start, end = 10):
         "color": color,
       }
       deck.append(card)
+
 
 def create_special_deck_cards(rang, symbol):
 
@@ -33,6 +32,24 @@ def create_special_deck_cards(rang, symbol):
       }
       deck.append(card)
 
+#choose bottom card, remove from deck and return the card
+def choose_random_bottom_card():
+  card = r.choice(deck)
+  #check if its not a special card
+  while not str(card["symbol"]).isnumeric():
+    card = r.choice(deck)
+  else:
+    deck.remove(card)
+    return card
+
+#shuffle deck and set bottom card
+def start_game():
+  #r.shuffle(deck)
+  pass
+
+
+#Net for control
+#for x in deck: print(x)
 
 create_deck_cards(0)  #0-9 cards
 create_deck_cards(1)  #1-9 cards
@@ -42,4 +59,18 @@ create_special_deck_cards(2, "draw 2")  #draw cards
 create_special_deck_cards(1, "wild")  #wild cards
 create_special_deck_cards(1, "wild draw 4") #wild draw cards
 
+# remember it's not accessed in fn(start fn)??? fixx, 
+bottom_card = choose_random_bottom_card()
+
+
+
+
+
+
+
+
+#net for control..
+start_game()
+print(bottom_card)
+print("xxxxx")
 for x in deck: print(x)
