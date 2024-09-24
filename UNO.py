b65ec0f3a2812bda80bf5408dc98f396705471e4
thembalakhe <m.themba1738@gmail.com>
2024-09-24 21:34:50 +0200

    added functionality for multi human player. Created a fn to call the next player. Improved the output of the  cli. Finally got the fn to create the Player Class instances automatically. Code refactor and improvements. Added a few more variables to accomodate the multi player functionality. MUST REMEMBER TO; store all global var to a single object. We are going somewhere finally!!!!

--- UNO.py
+++ UNO.py
@@ -2,119 +2,14 @@
 from random import choice, choices, shuffle
 
 colors = ["red", "yellow", "blue", "green"]
-deck: list[dict] = [] #{}
-bottom_card: dict = any
+deck: list[dict] = []
+bottom_card: dict[str | int] = any
+player_names: list[str] = ['Frank', 'Benjamin', 'Ducks']
+game_players: dict = {}
+current_player: int = 0
 
 
-#create cards and append to the deck array
-def create_deck_cards(start, end = 10) -> None:
-
-  for color in colors:
-    #
-    for number in range(start, end):
-      card = {
-        "symbol": number,
-        "color": color,
-      }
-      deck.append(card)
-
-
-#create special cards and append to the deck array
-def create_special_deck_cards(rang, symbol) -> None:
-
-  for color in colors:
-    #note wilds don't have a specific
-    if 'wild' in symbol:
-      color = "any"
-    #
-    for _ in range(rang):
-      card = {
-        "symbol": symbol,
-        "color": color,
-      }
-      deck.append(card)
-
-
-#fix this thing???
-#change bottom card
-def change_bottom_card(card) -> None:
-  global bottom_card 
-  bottom_card = card
-  
-
-#choose  bottom card, remove from deck and return the card
-def set_random_bottom_card() -> dict:
-
-  card: dict = choice(deck)
-
-  #check if its not a special card
-  while not str(card.get("symbol")).isnumeric():
-    card = choice(deck)
-  else:
-    change_bottom_card(card)
-    deck.remove(card) # remove card from deck
-    return card
-
-
-# this has not been used, do I even need it???,
-# maybe for/with played_a_special_card()
-def check_bottom_card_is_special() -> tuple[bool, str]:
-
-  symbol = bottom_card.get("symbol")
-  if str(symbol).isnumeric():
-    return False
-  else: return True, symbol
-
-
-#
-def reverse_card_played() -> None:
-
-  pass
-
-
-#
-def skip_card_played() -> None:
-  #increment by 2 once
-  #if reverse has been played decrement by 2
-  #the index of current player
-  pass
-
-
-#
-def draw_2_card_played() -> None:
-  #set the bottom card symbol and color = color
-  #return color
-
-  pass
-
-
-#
-def wild_draw_card_played() -> None:
-  #set the bottom card symbol and color = color
-  #return color
-  pass
-
-
-#
-def call_next_player() -> None:
-  pass 
-
-
-###########################################################
-#create and shuffle deck
-def start_game():
-
-  create_deck_cards(0)  #0-9 cards
-  create_deck_cards(1)  #1-9 cards
-  create_special_deck_cards(2, "skip")  #skip cards
-  create_special_deck_cards(2, "reverse") #reverse cards
-  create_special_deck_cards(2, "draw 2")  #draw cards
-  create_special_deck_cards(1, "wild color")  #wild cards
-  create_special_deck_cards(1, "wild draw 4") #wild draw cards
-  shuffle(deck)
-
-
-###################Class PLAYER########################################
+###################player class
 class Player():
 
   cards: list[dict] = []
@@ -123,33 +18,32 @@ class Player():
 
     self.name = name
     self.cards = choices(deck, k=7)
+    print("\nPlayer Name: ", self.name)
 
 
-  def check_if_winner(self):
-    if len(self.cards) < 1: exit()
+  def check_if_winner(self) -> None:
+    if len(self.cards) < 1:
+      exit(f"\nWE HAVE A WINNER: {self.name}")
 
   # 
   def display_player_cards(self) -> None: 
 
-    print("\nBottom Card: ", bottom_card, "\n")
+    print(f"\nBottom Card: {bottom_card} \n\n These Are Your Cards: ")
     
     counter = 0
 
-    for cards in self.cards:
+    for card in self.cards:
       counter += 1
-      print(counter, cards)
+      print(" ", counter, card)
 
+    print(" ", counter + 1, "Draw Card")
 
   #
-  def draw_cards(self, number_of_draws) -> None:
+  def draw_cards(self, number_of_draws: int) -> None:
 
     #select random cards from deck and add to player's cards
     cards = choices(deck, k=number_of_draws)
     for card in cards: self.cards.append(card)
-
-
-    #remove the selected cards from deck
-    for card in self.cards: deck.remove(card)
 
 
   #check if player has special card
@@ -167,8 +61,6 @@ class Player():
     counter = len(self.cards) + 1
 
     self.display_player_cards()
-      
-    print(counter , "Draw Card")
 
     user_choice = input("\nChoose a move to play: ")
 
@@ -178,16 +70,17 @@ class Player():
 
     return counter, int(user_choice)
 
-
   #
   def play_a_card(self) -> None:
+
+    print(f"\n{self.name} is now playing.")
 
     counter, user_choice = self.handle_player_options()
 
     #last list option will always be to draw cards
     if user_choice == counter:
       self.draw_cards(1)
-      print("You chose to draw a card.")
+      print("\nYou chose to draw a card.")
     else:
       player_card = self.cards[user_choice - 1]
 
@@ -202,18 +95,21 @@ class Player():
         else:
           change_bottom_card(player_card)
           self.cards.remove(player_card)
-          print("Correct, Next Player Please")
+
+          print("\nWell Played, Next Player Please")
 
       else:
-        print("\nTake +2 for your careless mistake! Nxa!")
         self.draw_cards(2)
       
-    self.display_player_cards()
+        print("\nTake +2 for your careless mistake! Nxa!")
+      
     self.check_if_winner()
+    #self.display_player_cards()
 
-  def played_a_special_card(self, card) -> None:
+  #
+  def played_a_special_card(self, card: dict) -> None:
 
-    print("\nSpecial card played, handling..")
+    print("\nSpecial card played")
 
   # "skip"  4x2 skip cards
   # "reverse" 4x2 reverse cards
@@ -227,31 +123,37 @@ class Player():
         
         case 'wild color':
             change_bottom_card(card)
-            #call next player
             print("\nAny Color Can Be Played!")
+            #call next player
 
         case 'skip':
             skip_card_played()
             change_bottom_card(card)
             print("\nSkipped Next Player, Askies")
+            #call next player
 
         case 'reverse':
             reverse_card_played()
             print("\nDirection Reversed, Hade Mfethu")
+            #call next player
 
         case "draw 2":
             draw_2_card_played()
             change_bottom_card(card)
             print("\nTake +2 Baba")
+            #call next player
 
         # default pattern
         case "wild draw 4":
             wild_draw_card_played()
             change_bottom_card(card)
             print("\nTake +4 or Fight Back?, Any Color!")
+            #call next player
+            
+    change_bottom_card(card)
 
 
-######################computer#####################
+######################computer classs
 
 # class Computer(Player):
 
@@ -259,19 +161,136 @@ class Player():
 #   def check_for_next_move():
 #     pass
 
+####################################end of class
 
-####################################end of class#####################
 
-####################################
+#create cards and append to the deck array
+def create_deck_cards(start: int, end: int = 10) -> None:
 
+  for color in colors:
+    #
+    for number in range(start, end):
+      card = {
+        "symbol": number,
+        "color": color,
+      }
+      deck.append(card)
+
+
+#create special cards and append to the deck array
+def create_special_deck_cards(rang: int, symbol: str) -> None:
+
+  for color in colors:
+    #note wilds don't have a specific
+    if 'wild' in symbol:
+      color = "any"
+    #
+    for _ in range(rang):
+      card = {
+        "symbol": symbol, "color": color,
+      }
+      deck.append(card)
+
+#
+def change_bottom_card(card: dict) -> None:
+  global bottom_card
+  bottom_card = card
+  
+
+#choose  bottom card, remove from deck and return the card
+def set_random_bottom_card() -> dict:
+
+  card: dict = choice(deck)
+
+  #check if its not a special card
+  while not str(card.get("symbol")).isnumeric():
+    card = choice(deck)
+  else:
+    change_bottom_card(card)
+    deck.remove(card)
+    return card
+
+
+# this has not been used, do I even need it???,
+# maybe for/with played_a_special_card()
+def check_bottom_card_is_special() -> tuple[bool, str | None]:
+
+  symbol = bottom_card.get("symbol")
+  if str(symbol).isnumeric():
+    return False
+  else: return True, symbol
+
+#
+def reverse_card_played() -> None:
+  pass
+
+#
+def skip_card_played() -> None:
+  #increment by 2 once
+  #if reverse has been played decrement by 2
+  #the index of current player
+  pass
+
+#
+def draw_2_card_played() -> None:
+  #set the bottom card symbol and color = color
+  #return color
+  pass
+
+#
+def wild_draw_card_played() -> None:
+  #set the bottom card symbol and color = color
+  #return color
+  pass
+
+#
+def call_next_player() -> None:
+  pass 
+
+
+#create and shuffle deck
+def start_game():
+
+  create_deck_cards(0)  #0-9 cards
+  create_deck_cards(1)  #1-9 cards
+  create_special_deck_cards(2, "skip")  #skip cards
+  create_special_deck_cards(2, "reverse") #reverse cards
+  create_special_deck_cards(2, "draw 2")  #draw cards
+  create_special_deck_cards(1, "wild color")  #wild cards
+  create_special_deck_cards(1, "wild draw 4") #wild draw cards
+  change_bottom_card(set_random_bottom_card())
+  shuffle(deck)
+
+
+#
+def create_players(player_names: list) -> dict:
+
+  global game_players
+
+  for name in player_names:
+      game_players[name] = Player(name)
+  return game_players
+
+#
+def call_player() -> None:
+
+  global current_player
+
+  if current_player < len(player_names):
+    (game_players.get(player_names[current_player])).play_a_card()
+    current_player += 1
+  if current_player == len(player_names):  current_player = 0
+
+  call_player()
+
+
+################################It Starts Here
 start_game()
-bottom_card = set_random_bottom_card()
-#bottom_card = {'symbol': "wild", 'color': 'green'}
+create_players(player_names)
+call_player()
 
-###################################
-
-
-### net for control ####
+###########################################
+###############net for control
 
 # print("bottom card: ", bottom_card)
 # print("number of cards: ", len(deck))
@@ -280,9 +299,9 @@ bottom_card = set_random_bottom_card()
 #print("bottom special card: ", check_bottom_card_is_special())
 
 
-player = Player("Frank Franklin")
+#player1 = Player("Frank Franklin")
+
 #player2 = Player("tshego")
-print("Player Name: ", player.name)
 #print("player cards: ", player2.name)
 #for card in player.cards: print(card)
 #print("player has special: ", player.player_has_special_card())
@@ -299,6 +318,3 @@ print("Player Name: ", player.name)
 #print("Tshego's turn")
 #player2.play_a_card()
 
-txt = {
-  'frank': player.play_a_card
-}
